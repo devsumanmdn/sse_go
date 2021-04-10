d7806e8fd5b3153394cba7a373e0bca03e9199b7
Suman Kundu <devsumanmdn@gmail.com>
2021-04-10 10:47:21 +0530

    Http3 up and running

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/devsumanmdn/sse_go
+
+go 1.16
+
+require github.com/lucas-clemente/quic-go v0.20.0
+
+require github.com/mitchellh/go-homedir v1.1.0
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+
+	"github.com/lucas-clemente/quic-go/http3"
+	"github.com/mitchellh/go-homedir"
+)
+
+func setupHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	wwwDir, _ := homedir.Expand("/Users/suman/go/sse_go/www")
+
+	fmt.Println(wwwDir)
+
+	mux.Handle("/", http.FileServer(http.Dir(wwwDir)))
+
+	return mux
+}
+
+// Start a server that echos all data on the first stream opened by the client
 func main() {
-	println("hey hi")
+
+	certFile, _ := homedir.Expand("~/localhost.crt")
+	keyFile, _ := homedir.Expand("~/localhost.key")
+
+	handler := setupHandler()
+
+	err := http3.ListenAndServe(":3000", certFile, keyFile, handler)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		log.Println("Running on https://locahost:3000")
+	}
 }
